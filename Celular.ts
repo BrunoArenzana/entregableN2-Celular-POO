4f40d332bf2da190a097ce661379f81fffa8fd84
escaliner <brunoarenzana@gmail.com>
2025-03-24 18:19:06 -0300

    Archivo Main y ClassCelular, 2do practico POO

---
+++ Celular.ts
@@ -0,0 +1,82 @@
+export class Celular {
+
+    private marca: string;
+    private modelo: string;
+    private ram: number;
+    private interStorage: number;
+    private sistOp: string;
+    private onOff: boolean;
+
+
+
+    constructor(
+        pMarca: string,
+        pModelo: string,
+        pSistOp: string,
+        pRam?: number,
+        pInterStorage?: number,
+        pOnOff?: boolean,
+        )  {      
+        this.marca = pMarca;
+        this.modelo = pModelo;
+        this.ram  = pRam ?? 4;
+        this.interStorage = pInterStorage ?? 64;
+        this.sistOp = pSistOp;
+        this.onOff = pOnOff ?? false;
+    }
+
+
+    public prenderApagar(): void {
+        this.onOff = !this.onOff; 
+        if (this.onOff) {
+            console.log("El sistema se ha encendido.");
+        } else {
+            console.log("El sistema se ha apagado.");
+        }
+    }
+
+
+    public mostrarInfo(): void {
+        console.log(`El celular ${this.marca} el modelo es ${this.modelo}, con una velocidad  de ${this.ram}Gb de Ram, un almacenamiento de ${this.interStorage} Gb y su sist operativo es ${this.sistOp} estado celular ${this.onOff} `)
+    }
+
+
+    public getpMarca(): string {
+        return this.marca;
+    }
+    public setpMarca(pMarca: string) {
+        this.marca = pMarca;
+    }
+
+    public getpModelo(): string {
+        return this.modelo;
+    }
+    public setpModelo(pModelo: string) {
+        this.modelo = pModelo;
+    }
+
+    public getpRam(): number {
+        return this.ram ?? 8;
+    }
+    public setpRam(pRam: number) {
+        this.ram = pRam;
+    }
+
+    public getpInterStorage(): number {
+        return this.interStorage?? 64;
+    }
+    public setpInterStorage(pInterStorage: number) {
+        this.interStorage = pInterStorage;
+    }
+
+    public getpSistOp(): string {
+        return this.sistOp;
+    }
+    public setpSistOp(pSistOp: string) {
+        this.sistOp = pSistOp;
+    }
+    public getOnOff(): boolean{
+        return this.onOff;
+    }
+
+}
---
+++ main.ts
@@ -0,0 +1,35 @@
+// Se debe crear una clase Celular que represente un teléfono móvil.
+//
+// Requisitos:
+// Encapsulamiento: Los atributos deben ser privados y accesibles solo mediante getters y setters.
+// Constructor con parámetros opcionales y obligatorios:
+// Obligatorios: Marca, modelo y sistema operativo.
+//     Opcionales: Memoria RAM(por defecto 4GB), almacenamiento interno(por defecto 64GB) y 
+// //estado de encendido(por defecto apagado).
+// Métodos adicionales:
+// encenderApagar(): Cambia el estado de encendido / apagado del celular.
+//     mostrarInfo(): Retorna una cadena con los datos del celular.
+
+// GRUPO DE TRABAJO
+
+// GARCIA MARCELO , ARENZANA BRUNO , MANUEL IRYGOYEN
+import { Celular } from "./Celular";
+
+// caso 1 completo a mano los valores
+const cel1 = new Celular("Samsumg", "A 22", "Android 12.8", 8, 256, true);
+//caso 2 dejo el constructor sin los opcionales y carga por defecto 4 gb ram, 64 almacenamiento y False en prendido/ apagado.
+const cel2 = new Celular("Iphone", "14 Pro", "Ios 16.8");
+//caso 3 completo los opcionales con "undefined"
+const cel3 = new Celular("Xiaomi ReadMi", "Pro 13", "Android 14.4", undefined, undefined, undefined);
+
+
+console.log("-------Celular numero 1--------------------------------")
+cel1.mostrarInfo();
+console.log("-------Celular numero 2--------------------------------")
+cel2.mostrarInfo();
+console.log("-------Celular numero 3--------------------------------")
+cel3.mostrarInfo();
+console.log("-----------Prender Apagar- celular 1 , 2 y 3 ----------")
+console.log("celular 1 "); cel1.prenderApagar();
+console.log("celular 2 "); cel2.prenderApagar();
+console.log("celular 2 "); cel3.prenderApagar();
